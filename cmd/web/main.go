d6e448a9a821d712d37935bac569e07d25f92d48
alekslesik <alekslesik@gmail.com>
2022-08-23 16:18:58 +0300

    Setting Up the Session Manager

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,8 +7,10 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/alekslesik/snippetbox.learn/pkg/models/mysql"
+	"github.com/golangcollege/sessions"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -16,47 +18,56 @@ import (
 type application struct {
 	errorLog      *log.Logger
 	infoLog       *log.Logger
+	session       *sessions.Session
 	snippets      *mysql.SnippetModel
 	templateCache map[string]*template.Template
 }
 
 func main() {
+	// Command-line flag parsing
 	addr := flag.String("addr", ":4000", "Сетевой адрес веб-сервера")
 	dsn := flag.String("dsn", "web:ndJMv9zrJw@/snippetbox?parseTime=true", "Название MySQL источника данных")
+	secret := flag.String("secret", "s6Ndh+pPbnzHbS*+9Pk8qGWhTzbpa@ge", "Secret")
 	flag.Parse()
 
+	// Loggers
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
+	// Open DB connection pull
 	db, err := openDB(*dsn)
 	if err != nil {
 		errorLog.Fatal(err)
 	}
-
 	defer db.Close()
 
-	// initialise new cache pattern
-	// templateCache, err := newTemplateCache("./ui/html")
-	// for debug
+	// Initialise new cache pattern
 	templateCache, err := newTemplateCache("C:/Users/Lesik/go/src/github.com/alekslesik/snippetbox.learn/ui/html")
 	if err != nil {
 		errorLog.Fatal(err)
 	}
 
+	// Initialize a new session manager
+	session := sessions.New([]byte(*secret))
+	session.Lifetime = 12 * time.Hour
+
+	// Initialisation application struct
 	app := &application{
 		errorLog:      errorLog,
 		infoLog:       infoLog,
+		session:       session,
 		snippets:      &mysql.SnippetModel{DB: db},
 		templateCache: templateCache,
 	}
 
+	// Confugure server
 	srv := &http.Server{
 		Addr:     *addr,
 		ErrorLog: errorLog,
 		Handler:  app.routes(),
 	}
 
-	infoLog.Printf("Запуск сервера на http://127.0.0.1%s", *addr)
+	infoLog.Printf("Server started on http://127.0.0.1%s", *addr)
 	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
 }
