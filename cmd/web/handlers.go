18079b74b8731b72efbd5214a64e7f073d5f89f9
alekslesik <alkslesik@gmail.com>
2022-08-31 15:41:04 +0300

    Validating the User Input

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -95,6 +95,28 @@ func (app *application) signupUserForm(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) signupUser(w http.ResponseWriter, r *http.Request) {
+	// Parse the form data.
+	err := r.ParseForm()
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+
+	// Validate the form contents using the form helper we made earlier.
+	form := forms.New(r.PostForm)
+	form.Required("name", "email", "password")
+	form.MatchesPattern("email", forms.EmailRX)
+	form.MinLength("password", 10)
+
+	// If there are any errors, redisplay the signup form.
+	if !form.Valid() {
+		app.render(w, r, "signup.page.html", &templateData{
+			Form: form,
+		})
+		return
+	}
+
+	// Otherwise send a placeholder response (for now!).
 	fmt.Fprintln(w, "Create a new user...")
 }
 
